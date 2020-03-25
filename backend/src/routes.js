6973c4bd037288be9a4c9cd836e0bed499304a9e
Cintia Neves <maria.cintia@rhfranquias.com.br>
2020-03-24 22:18:00 -0300

    Criação do banco de dados sqlite3

---
+++ backend/src/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
+
+const routes = express.Router();
+
+
+routes.post('/ongs', async (request, response) => {
+
+    const {name, email, whatsapp, city, uf} = request.body;
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        city,
+        whatsapp,
+        uf,
+    })
+
+    return response.json({id});
+});
+
+module.exports = routes;
